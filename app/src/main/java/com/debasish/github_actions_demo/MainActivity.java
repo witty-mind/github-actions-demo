9cb812aefa6d2530cf7cdfd518709406db93a8e9
chicky <shwetananda2011@gmail.com>
2021-03-06 16:41:21 +0530

    my number commit

--- app/src/main/java/com/debasish/github_actions_demo/MainActivity.java
+++ app/src/main/java/com/debasish/github_actions_demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.debasish.github_actions_demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getPrint();
+    }
+
+    private void getPrint() {
+        for (int i=0;i<=100;i++)
+        {
+            Log.i("print",""+i);
+        }
     }
 }
